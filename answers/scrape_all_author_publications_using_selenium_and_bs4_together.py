2028485edfd049174b7e2d897b59b0a5c477f4c0
Dimitry Zub <dimitryzub@gmail.com>
2022-02-23 15:10:15 +0200

    add saving option to scrape_all_author_publications_using_selenium_and_bs4_together.py

--- answers/scrape_all_author_publications_using_selenium_and_bs4_together.py
+++ answers/scrape_all_author_publications_using_selenium_and_bs4_together.py
@@ -1,6 +1,7 @@
 # https://stackoverflow.com/questions/63322113/using-selenium-and-beautiful-soup-together
 # https://replit.com/@DimitryZub1/using-selenium-and-beautiful-soup-together
 
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests, lxml, os
 from serpapi import GoogleSearch
@@ -9,16 +10,18 @@ from urllib.parse import urlsplit, parse_qsl
 
 def bs4_scrape_articles():
     params = {
-        "user": "VjJm3zYAAAAJ",  # user-id
-        "hl": "en",  # language
-        "gl": "us",  # country to search from
-        "cstart": 0,  # articles page. 0 is the first page
-        "pagesize": "100"  # articles per page
+        "user": "VxOmZDgAAAAJ",       # user-id
+        "hl": "en",                   # language
+        "gl": "us",                   # country to search from
+        "cstart": 0,                  # articles page. 0 is the first page
+        "pagesize": "100"             # articles per page
     }
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3538.102 Safari/537.36 Edge/18.19582",
     }
+
+    all_articles = []
 
     articles_is_present = True
 
@@ -38,6 +41,13 @@ def bs4_scrape_articles():
                   article_authors,
                   article_publication, sep="\n")
 
+            all_articles.append({
+                "title": article_title,
+                "link": article_link,
+                "authors": article_authors,
+                "publication": article_publication
+            })
+
         # this selector is checking for the .class that contains: "There are no articles in this profile."
         # example link: https://scholar.google.com/citations?user=VjJm3zYAAAAJ&hl=en&cstart=500&pagesize=100
         if soup.select_one(".gsc_a_e"):
@@ -45,18 +55,22 @@ def bs4_scrape_articles():
         else:
             params["cstart"] += 100  # paginate to the next page
 
+    pd.DataFrame(data=all_articles).to_csv(f"google_scholar_{params['user']}_articles.csv", encoding="utf-8", index=False)
+
 
 def serpapi_scrape_articles():
     params = {
         "api_key": os.getenv("API_KEY"),
         "engine": "google_scholar_author",
         "hl": "en",
-        "author_id": "VjJm3zYAAAAJ",
+        "author_id": "VxOmZDgAAAAJ",
         "start": "0",
         "num": "100"
     }
 
     search = GoogleSearch(params)
+
+    all_articles = []
 
     articles_is_present = True
 
@@ -77,8 +91,18 @@ def serpapi_scrape_articles():
                   publication,
                   citation_id, sep="\n")
 
-        if "next" in results["serpapi_pagination"]:
+            all_articles.append({
+                "title": title,
+                "link": link,
+                "authors": authors,
+                "publication": publication,
+                "citation_id": citation_id
+            })
+
+        if "next" in results.get("serpapi_pagination", []):
             # split URL in parts as a dict() and update "search" variable to a new page
             search.params_dict.update(dict(parse_qsl(urlsplit(results["serpapi_pagination"]["next"]).query)))
         else:
             articles_is_present = False
+
+    pd.DataFrame(data=all_articles).to_csv(f"serpapi_google_scholar_{params['author_id']}_articles.csv", encoding="utf-8", index=False)
