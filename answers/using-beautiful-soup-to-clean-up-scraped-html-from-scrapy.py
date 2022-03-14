2c7746457707a044134c829cfc97eec48f07304f
Dimitry Zub <dimitryzub@gmail.com>
2022-03-14 18:43:51 +0200

    add using-beautiful-soup-to-clean-up-scraped-html-from-scrapy.py

---
+++ answers/using-beautiful-soup-to-clean-up-scraped-html-from-scrapy.py
@@ -0,0 +1,31 @@
+# https://stackoverflow.com/questions/21839877/using-beautiful-soup-to-clean-up-scraped-html-from-scrapy
+# https://replit.com/@DimitryZub1/using-beautiful-soup-to-clean-up-scraped-html-from-scrapy#main.py
+
+import scrapy
+
+class ScholarSpider(scrapy.Spider):
+    name = "scholar_titles"
+    allowed_domains = ["scholar.google.com"]
+    start_urls = ["https://scholar.google.com/scholar?q=intitle%3Apython+xpath"]
+
+    def parse(self, response):
+        for quote in response.xpath('//*[@class="gs_rt"]/a'):
+            yield {
+                "title": quote.xpath("normalize-space()").get()
+            }
+
+from serpapi import GoogleScholarSearch
+
+params = {
+  "api_key": "Your SerpApi API key",
+  "engine": "google_scholar",   # parsing engine
+  "q": "intitle:python xpath",  # search query
+  "hl": "en"                    # language
+}
+
+search = GoogleScholarSearch(params)
+results = search.get_dict()
+
+for result in results["organic_results"]:
+    title = result["title"]
+    print(title)
