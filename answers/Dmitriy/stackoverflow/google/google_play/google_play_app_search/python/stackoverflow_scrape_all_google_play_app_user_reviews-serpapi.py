df2707ccc0cb57a92c13e1ce6ae3cf61b1cd971f
Dmitriy <dmitriy@serpapi.com>
2022-04-19 15:35:14 +0300

    update stackoverflow_scrape_all_google_play_app_user_reviews-serpapi.py

--- answers/Dmitriy/stackoverflow/google/google_play/google_play_app_search/python/stackoverflow_scrape_all_google_play_app_user_reviews-serpapi.py
+++ answers/Dmitriy/stackoverflow/google/google_play/google_play_app_search/python/stackoverflow_scrape_all_google_play_app_user_reviews-serpapi.py
@@ -2,15 +2,16 @@ from serpapi import GoogleSearch
 from urllib.parse import urlsplit, parse_qsl
 import json, os
 
-def serpapi_scrape():
+
+def serpapi_scrape_reviews():
     # https://docs.python.org/3/library/os.html#os.getenv
     params = {
-      "api_key": os.getenv("API_KEY"),     # your serpapi api key
-      "engine": "google_play_product",     # search engine
-      "store": "apps",                     # 
-      "gl": "es",                          # country to search from: Spain
-      "product_id": "com.nintendo.zara",   # app ID
-      "all_reviews": "true"                # show all reviews
+        "api_key": os.getenv("API_KEY"),     # your serpapi api key
+        "engine": "google_play_product",     # search engine
+        "store": "apps",                     # 
+        "gl": "es",                          # country to search from: Spain
+        "product_id": "com.nintendo.zara",   # app ID
+        "all_reviews": "true"                # show all reviews
     }
     
     search = GoogleSearch(params)          # where data extraction happens
@@ -20,15 +21,15 @@ def serpapi_scrape():
         print(json.dumps(review, indent=2))
 
 
-def serpapi_scrape_pagination():
+def serpapi_scrape_all_reviews():
     # https://docs.python.org/3/library/os.html#os.getenv
     params = {
-      "api_key": os.getenv("API_KEY"),     # your serpapi api key
-      "engine": "google_play_product",     # search engine
-      "store": "apps",                     
-      "gl": "es",                          # country to search from: Spain
-      "product_id": "com.nintendo.zara",   # app ID
-      "all_reviews": "true"                # show all reviews
+        "api_key": os.getenv("API_KEY"),     # your serpapi api key
+        "engine": "google_play_product",     # search engine
+        "store": "apps",                     
+        "gl": "es",                          # country to search from: Spain
+        "product_id": "com.nintendo.zara",   # app ID
+        "all_reviews": "true"                # show all reviews
     }
     
     search = GoogleSearch(params)          # where data extraction happens
