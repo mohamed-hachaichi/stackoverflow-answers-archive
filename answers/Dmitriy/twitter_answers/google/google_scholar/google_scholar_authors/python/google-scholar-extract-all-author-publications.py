325ac7343fb455f1f6a1e891a476a9db73d8e472
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-05-05 11:04:58 +0300

    add serpapi solution

--- answers/Dmitriy/twitter_answers/google/google_scholar/google_scholar_authors/python/google-scholar-extract-all-author-publications.py
+++ answers/Dmitriy/twitter_answers/google/google_scholar/google_scholar_authors/python/google-scholar-extract-all-author-publications.py
@@ -2,6 +2,11 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests, lxml, os
 
+from serpapi import GoogleScholarSearch
+from urllib.parse import urlsplit, parse_qsl
+
+
+
 def scrape_all_authors_articles(author_id: str):
     params = {
         "user": author_id,   # user-id
@@ -64,3 +69,58 @@ user_ids = ["rUHfmpQAAAAJ", "HRkZPK4AAAAJ", "DubfkQ8AAAAJ", "VjJm3zYAAAAJ"]
 
 for _id in user_ids:
     scrape_all_authors_articles(author_id=_id)
+    
+    
+# SerpApi solution
+
+import os
+
+
+
+def serpapi_scrape_all_author_articles(author_id: str):
+    params = {
+        "api_key": os.getenv("API_KEY"),    # your SerpApi API key
+        "engine": "google_scholar_author",  # search engine
+        "hl": "en",                         # language
+        "author_id": author_id,             # author ID
+        "start": "0",                       # articles page
+        "num": "100"                        # articles per page
+        }
+
+    search = GoogleScholarSearch(params)    # where data extraction happens on SerpApi backend.
+
+    all_articles = []
+
+    articles_is_present = True
+    while articles_is_present:
+        results = search.get_dict()         # JSON -> Python dictionary
+
+        for index, article in enumerate(results["articles"], start=1):
+            title = article.get("title")
+            link = article.get("link")
+            authors = article.get("authors")
+            publication = article.get("publication")
+            citation_id = article.get("citation_id")
+            year = article.get("year")
+
+            print(title)
+
+            all_articles.append({
+                "title": title,
+                "link": link,
+                "authors": authors,
+                "publication": publication,
+                "citation_id": citation_id,
+                "year": year
+                })
+
+        if "next" in results.get("serpapi_pagination", []):
+            # split URL in parts as a dict() and update "search" variable to a new page
+            search.params_dict.update(dict(parse_qsl(urlsplit(results["serpapi_pagination"]["next"]).query)))
+        else:
+            articles_is_present = False
+
+    pd.DataFrame(data=all_articles).to_csv(f"serpapi_google_scholar_{params['author_id']}_articles.csv", encoding="utf-8", index=False)
+
+
+serpapi_scrape_all_author_articles(author_id="VjJm3zYAAAAJ")
